2c94f482e6d83032a9399d80f0cb1c5a402138a2
sri-narendra <dmukeshmanoj@gmail.com>
2026-05-29 02:45:38 +0530

    Remove hardcoded MongoDB credentials from config

--- config/database.php
+++ config/database.php
@@ -5,8 +5,8 @@ return [
     'connections' => [
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('MONGODB_DSN', 'mongodb+srv://dmukeshmanoj_db_user:Mukesh%402006@cluster0.sew8ubi.mongodb.net/appointment-system'),
-            'database' => env('DB_DATABASE', 'appointment-system'),
+            'dsn' => env('MONGODB_DSN'),
+            'database' => env('DB_DATABASE'),
         ],
     ],
     'migrations' => 'migrations',
